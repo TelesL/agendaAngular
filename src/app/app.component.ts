bf4a12f4876ba04d35e454792ae21f9a9e3f0437
Lucas Teles <lucasspteles@outlook.com>
2024-09-19 22:34:04 -0300

    project-updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,21 @@
-import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ContainerComponent } from './componentes/container/container.component';
-import { ContatoComponent } from './componentes/contato/contato.component';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { SeparadorComponent } from './componentes/separador/separador.component';
-
-interface Contato {
-  id: number,
-  nome: string,
-  telefone: string
-}
-
-import agenda from './agenda.json'
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule, 
-    RouterOutlet,
-    ContainerComponent,
-    CabecalhoComponent,
-    SeparadorComponent,
-    ContatoComponent,
-    FormsModule
+    FormularioContatoComponent,
+    ListaContatosComponent,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
-  contatos: Contato[] = agenda;
-
-  filtroPorTexto: string =''
-
-  filtrarContatosPorTexto (): Contato [] {
-    if (!this.filtroPorTexto) {
-      return this.contatos
-    }
-    return this.contatos.filter(contato => {
-      return contato.nome.toLowerCase(). includes(this.filtroPorTexto.toLowerCase())
-    })
-  }
-
-  filtrarContatosPorLetraInicial(letra:string): Contato [] {
-    return this.filtrarContatosPorTexto().filter( contato => {
-      return contato.nome.toLowerCase().startsWith(letra)
-    });
-  }
+ 
 }
